cfb47e8a340075099666bafd6bcaff2fffba1129
Per Kops <pko@delegate.dk>
2022-01-06 16:38:08 +0100

    Utilize expression-bodied syntax for WriteHeader method in ConsoleHelper

--- src/Atc.Iot.DigitalTwin.Cli/ConsoleHelper.cs
+++ src/Atc.Iot.DigitalTwin.Cli/ConsoleHelper.cs
@@ -3,7 +3,5 @@ namespace Atc.Iot.DigitalTwin.Cli;
 public static class ConsoleHelper
 {
     public static void WriteHeader()
-    {
-        AnsiConsole.Write(new FigletText("DTDL CLI").Color(Color.CornflowerBlue));
-    }
+        => AnsiConsole.Write(new FigletText("DTDL CLI").Color(Color.CornflowerBlue));
 }
