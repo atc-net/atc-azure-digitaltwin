2a64c457c870c76f86e034942032113a5cd1e69e
Per Kops <pko@delegate.dk>
2024-05-02 11:29:32 +0200

    feat: add TenantId to ConnectionBaseCommandSettings

--- src/Atc.Azure.DigitalTwin.CLI/Commands/Settings/ConnectionBaseCommandSettings.cs
+++ src/Atc.Azure.DigitalTwin.CLI/Commands/Settings/ConnectionBaseCommandSettings.cs
@@ -2,12 +2,21 @@ namespace Atc.Azure.DigitalTwin.CLI.Commands.Settings;
 
 public class ConnectionBaseCommandSettings : BaseCommandSettings
 {
+    [CommandOption("--tenantId <TENANTID>")]
+    [Description("Azure Tenant Id")]
+    public string? TenantId { get; init; }
+
     [CommandOption("-a|--adtInstanceUrl <ADTINSTANCEURL>")]
     [Description("Azure Digital Twin Instance Url")]
     public string? AdtInstanceUrl { get; init; }
 
     public override ValidationResult Validate()
     {
+        if (string.IsNullOrWhiteSpace(TenantId))
+        {
+            return ValidationResult.Error($"{nameof(TenantId)} must be present.");
+        }
+
         if (string.IsNullOrWhiteSpace(AdtInstanceUrl))
         {
             return ValidationResult.Error($"{nameof(AdtInstanceUrl)} must be present.");
